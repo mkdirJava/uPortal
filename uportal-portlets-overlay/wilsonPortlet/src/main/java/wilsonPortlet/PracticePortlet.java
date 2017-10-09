0751a6a51aca3e4d9822f92c6f654929bdaa8b43
Wilson Li <Wilson.Li@uwe.ac.uk>
2017-10-09 14:45:09 +0100

    portlet code in

---
+++ uportal-portlets-overlay/wilsonPortlet/src/main/java/wilsonPortlet/PracticePortlet.java
@@ -0,0 +1,53 @@
+package wilsonPortlet;
+
+import static javax.portlet.PortletSession.APPLICATION_SCOPE;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletSession;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+
+@Controller("wilsonController")
+@RequestMapping("VIEW")
+public class PracticePortlet {
+
+	@RenderMapping
+	protected ModelAndView doView(RenderRequest request, RenderResponse response) throws IOException, PortletException {
+
+		response.setContentType("text/html");
+
+		// Get preferences & session
+		PortletSession session = request.getPortletSession();
+
+		// Get username & noteid
+		String username = (String) ((Map) request.getAttribute(PortletRequest.USER_INFO)).get("dbUid");
+		session.setAttribute("username", username, APPLICATION_SCOPE);
+
+		return new ModelAndView("practice");
+	}
+
+	@ActionMapping
+	public void processAction(ActionRequest request, ActionResponse response) throws IOException, PortletException {
+		System.out.println("Hi There in the process action");
+	}
+
+	@ResourceMapping
+	public void serveResource(ResourceRequest request, ResourceResponse response) throws PortletException, IOException {
+		System.out.println("hi there in the Server Resource");
+	}
+
+}
